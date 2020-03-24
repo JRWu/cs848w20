19489f55b15029b53630c6553c61c0c6d421ea32
Jia Wu <jwu424@gmail.com>
2020-03-24 12:07:53 -0400

    Updating to contain config for running magellan.

---
+++ src/running_magellan.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# https://github.com/anhaidgroup/py_entitymatching
+# https://sites.google.com/site/anhaidgroup/projects/magellan/py_entitymatching
+# A collection of commands used to get Magellan up & running through Docker
+
+# Compile the docker container
+make all
+
+# Shell into the docker container
+make magellan
+
+# Within the docker container
+# From /src/magellan
+cd /magellan/py_entitymatching-master/
+
+# Install the dependencies
+pip install -r requirements.txt
+
+# Build
+python setup.py build
+
+# Setup the program
+python setup.py install
+
+# Return to the root dir to execute code from running_magellan.py
+cd /root
